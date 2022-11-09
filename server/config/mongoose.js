2a7f5a6d7ee63e04ac0c006f3697db5964a6cbfe
wendy-a <wendy200999@gmail.com>
2022-11-09 12:39:32 -0800

    update code
    remove outdated

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = (DB) => {
-    mongoose.connect("mongodb+srv://wendy:xls110110@cluster0.ocxxy.mongodb.net/" +
+    mongoose.connect("mongodb+srv://<username>:<password>@cluster0.ocxxy.mongodb.net/" +
             DB + "?retryWrites=true&w=majority")
         .then(() => console.log("Established a connection to the database"))
         .catch(err => console.log("Something went wrong when connecting to the database", err))
